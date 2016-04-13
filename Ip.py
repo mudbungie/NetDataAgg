c154a4ff19f02ae69ed0cea81b0ee124e80ff270
mudbungie <mudbungie@gmail.com>
2016-04-13 09:22:03 -0700

    implemented improved Ip class

--- Ip.py
+++ Ip.py
@@ -1,30 +1,29 @@
-# Class for an IP address, which is the only piece of network data that we 
-# can actually expect to be unique.
+# Subscripting a string for IP validation, because (jingle) always validate...
+# your input!
 
-import re
-from ipaddress import IPv4Address
-
-class Ip:
-    def __new__(self, ip, encoding=None):
-        # If it's an ipaddress, that's fine. Otherwise, validate.
+class Ip(str):
+    def __new__(cls, address, encoding=None):
+        # Figure out what we're being passed, convert anything to strings.
         if not encoding:
-            self.ip = ip
+            # For just strings
+            pass
         elif encoding == 'snmp':
-            # The encoding for SNMP is all malformed.
-            print(ip)
-            ipstr = '.'.join(ip.split('.')[1:])    
-            # If it's a string, we need to validate it.
-            #ipre = re.compile(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}\
-            #    (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
-            ipre = re.compile(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
-            if not ipre.match(ipstr):
-                print('ip: ' + ipstr)
-                raise Exception('Not an IP address')
-            self.ip = IPv4Address(ipstr)
+            # Returns from SNMP come with a leading immaterial number.
+            address = '.'.join(address.split('.')[1:])
         else:
-            # Should never happen
-            raise Exception('Unsupported encoding ' + encoding)
-        return self.ip
+            # Means invalid encoding passed.
+            raise Exception('Improper encoding passed with IP address')
 
-    #def __str__(self):
-    #    return str(self.ip)
+        # Now validate!
+        # Split the address into its four octets
+        ipBytes = [int(b) for b in address.split('.')]
+        # Throw out anything that isn't a correct octet.
+        ipBytes = [b for b in ipBytes if 0 <= b < 256]
+        ipStr = '.'.join([str(b) for b in ipBytes])
+        # Make sure that it has four octets, and that we haven't lost anything.
+        if len(ipBytes) != 4 or ipStr != address:
+            raise Exception('Improper string submitted for IP address')
+
+        # Sound like everything's fine!
+        return super(Ip, cls).__new__(cls, address)
+
